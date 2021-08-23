c3351cae98a29677bf80f2c72a7283753fadb264
ricl <riclavaz@gmail.com>
2021-08-23 17:38:38 +0800

    generate from https://start.spring.io/

---
+++ src/test/java/com/example/skeleton/SkeletonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.skeleton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkeletonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
