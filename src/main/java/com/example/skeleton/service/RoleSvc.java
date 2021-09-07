ef4a60913d003b9130e73a52795ecb528c2e7477
ricl <riclavaz@gmail.com>
2021-09-07 09:34:20 +0800

    fix: role maping when null

--- src/main/java/com/example/skeleton/service/RoleSvc.java
+++ src/main/java/com/example/skeleton/service/RoleSvc.java
@@ -8,6 +8,7 @@ import org.springframework.validation.annotation.Validated;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -41,6 +42,9 @@ public class RoleSvc extends CrudSvc<Role, Long> {
 
     @Validated
     public @NotNull Set<Role> MapByID(Set<Long> id) {
+        if (id == null) {
+            return new HashSet<>();
+        }
         return id.stream().map(this::GetByID).collect(Collectors.toSet());
     }
 
